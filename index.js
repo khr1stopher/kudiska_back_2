600c3395c2b5d74691f4edd957b93cde04765203
khr1stopher <kkromans009@gmail.com>
2020-09-14 23:17:45 -0400

    un cambio arrecho

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ app.get('/', function(req, res){
    res.send("Hello world!");
 });
 
-const httpsServer = https.createServer(credentials, app);
-
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(8000)
-console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+var server = app.listen(3000, () => {
+   console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
