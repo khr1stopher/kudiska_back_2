97bde0daf98c7798e8c450016500bf8c56b0ca57
khr1stopher <kkromans009@gmail.com>
2020-09-16 23:23:27 -0400

    mail_funcionhando

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ app.get('*', (req, res) => {
 });
 
 app.post('/user/add', (req, res) => {
-   usuario = req.body
-   sql = `INSERT INTO users (\`id\`,\`firstname\`, \`lastname\`, \`years_old\`, \`phone\`,\`email\`, \`password\`, \`time_create\`) VALUES (NULL, '${usuario.firstname}', '${usuario.lastname}', ${usuario.years_old}, '${usuario.phone}', '${usuario.email}', '${usuario.password}', '${new Date().toISOString()}')`
-   respuesta = 'nada'
-   con.query(sql, (err, result) => {
+   usuario = req.body;
+   usuario.time_create = new Date().toISOString()
+   sql = `INSERT INTO users (\`id\`, \`firstname\` ,  \`lastname\` ,  \`years_old\` ,  \`phone\` , \`email\` ,  \`password\` ,  \`time_create\` ) VALUES (?)`
+   con.query(sql,usuario,(err, result) => {
       if (err) throw err;
       res.status(200)
       res.json(result);
@@ -75,37 +75,39 @@ var server = app.listen(puerto, () => {
    console.log(`Express server ${puerto}: \x1b[32m%s\x1b[0m`, 'online');
 });
 
-async function mail_start() {
+async function send_mail(De = "LanzaApp", para, asunto, msg) {
    // Generate test SMTP service account from ethereal.email
    // Only needed if you don't have a real mail account for testing
    // let testAccount = await nodemailer.createTestAccount();
  
    // create reusable transporter object using the default SMTP transport
    let transporter = nodemailer.createTransport({
-     host: "http://smtp.kudiska.com",
+     host: "smtp.gmail.com",
      port: 587,
      secure: false, // true for 465, false for other ports
      auth: {
-       user: 'admin@kudiska.com', // generated ethereal user
-       pass: 'y,3]yqD4Vqau', // generated ethereal password
+       user: 'lanzapp.com@gmail.com', // generated ethereal user
+       pass: 'rbihvphguseuqfvf', // generated ethereal password
      },
    });
  
+// http://api.ipify.org/?format=jsonp&callback=JSONP_CALLBACK
+
    // send mail with defined transport object
    let info = await transporter.sendMail({
-     from: '"hola mundo 👻" <admin@kudiska.com>', // sender address
-     to: "kpineda18@outlook.com, kkromans009@gmail.com", // list of receivers
-     subject: "Hello ✔", // Subject line
-     text: "Hello world?", // plain text body
-     html: "<b>Hello world?</b>", // html body
+     from: `${De} <admin@kudiska.com>`, // sender address
+     to: para, // list of receivers
+     subject: asunto, // Subject line
+   //   text: "Hello world?", // plain text body
+     html: msg, // html body
    });
  
    console.log("Message sent: %s", info.messageId);
    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
  
    // Preview only available when sending through an Ethereal account
-   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+   // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
  }
  
- mail_start().catch(console.error);
+ send_mail("LanzaApp", "kpineda18@outlook.com","probando mailer", "<b>probando si manda correo</b>").catch(console.error);
